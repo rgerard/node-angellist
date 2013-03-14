2fd988ab3d26f2ffe20657fb077bd8fa63ebd358
rgerard <ryan.gerard@gmail.com>
2013-03-14 00:09:26 -0700

    Cleaning up some of the calls for /me

--- index.js
+++ index.js
@@ -21,18 +21,48 @@ function init(clientID, secret) {
     config.secret = secret;
 }
 
-function authorize(callback) {
+function setAccessToken(token) {
+    config.access_token = token;
+}
+
+function getAuthUrl() {
     var url = 'https://angel.co/api/oauth/authorize?client_id=' + config.clientID + '&scope=email&response_type=code';
-    createRequest(url, callback);
+    return url;
 }
 
 function requestAccessToken(code, callback) {
     var url = 'https://angel.co/api/oauth/token?client_id=' + config.clientID + '&client_secret=' + config.secret + '&code=' + code + '&grant_type=authorization_code';
-    createRequest(url, callback);
+    createPostRequest(url, '', callback);
+}
+
+function getMe(callback) {
+
+    // If the access token isn't set, return an error
+    if(!config.hasOwnProperty('access_token') || config.access_token === null || config.access_token === '') {
+        callback({'error':'no access token'}, null);
+    } else {
+        var url = 'https://api.angel.co/1/me?access_token=' + config.access_token;
+        createRequest(url, callback);
+    }
 }
 
 function createRequest(url, callback) {
     request(url, function (error, response, body) {
+
+        if (response.statusCode == 200) {
+            callback(error, JSON.parse(body));
+        } else {
+            callback(error, body);
+        }
+    });
+}
+
+function createPostRequest(url, body, callback) {
+    request.post({
+        headers: {'content-type' : 'application/x-www-form-urlencoded'},
+        url:     url,
+        body:    body
+    }, function(error, response, body){
 
         if (response.statusCode == 200) {
             callback(error, JSON.parse(body));
@@ -47,11 +77,19 @@ module.exports = {
         return init(clientID, secret);
     },
 
-    authorize: function(callback) {
-        return authorize(callback);
+    setAccessToken: function(token) {
+        return setAccessToken(token);
+    },
+
+    getAuthUrl: function() {
+        return getAuthUrl();
     },
 
     requestAccessToken: function(code, callback) {
         return requestAccessToken(code, callback);
+    },
+
+    getMe: function(callback) {
+        return getMe(callback);
     }
 }
